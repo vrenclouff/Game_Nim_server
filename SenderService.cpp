3608f454657925637313f374f9b4b283b41a57bd
Lukas Cerny <luc.cerny@gmail.com>
2017-01-03 16:01:57 +0100

    no message

--- SenderService.cpp
+++ SenderService.cpp
@@ -12,18 +12,23 @@ void SenderService::run()
     {
         logger->debug("Waiting for send message.");
         SNDMessage message = send_queue->pop();
-        std::string msg = createValidMessage(StringUtils::format(3, EnumUtils::to_string(message.state).c_str(), " ", message.parameters.c_str()));
+
+        std::string string_state = EnumUtils::to_string(message.state);
+        std::string string_parameters =  message.parameters;
+
+        std::string msg = createValidMessage(string_state + std::string(" ") + string_parameters);
         if (message.state != enums::PONG)
         {
             logger->info(StringUtils::format(4, "Sending to user: ", std::to_string(message.socket).c_str() ," message: ", msg.c_str()));
         }
+        if (msg.length() == 0) continue;
         send(message.socket, msg.c_str(), msg.length(), 0);
     }
 }
 
 std::string SenderService::createValidMessage(std::string const &msg)
 {
-    char buf[msg.length()+3];
+    char buf[msg.length()+4];
     int i;
     long check_sum = 0;
     char c;
@@ -34,8 +39,11 @@ std::string SenderService::createValidMessage(std::string const &msg)
         buf[i] = c;
         check_sum += c;
     }
+
     buf[0] = check_sum % CHECKSUM;
+    if (buf[0] == 0) buf[0] = CHECKSUM;
     buf[1] = STX;
     buf[i] = ETX;
+    buf[i+1] = '\0';
     return buf;
 }
